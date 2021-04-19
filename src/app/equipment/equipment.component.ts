0d1f9a367d7950a3540913fd1700da279e37bd2c
Justin Portell <72322829+jupo-ex-it@users.noreply.github.com>
2021-04-18 21:53:17 -0500

    30.7.2.2 complete

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,31 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-equipment',
-  templateUrl: './equipment.component.html',
-  styleUrls: ['./equipment.component.css']
+    selector: 'app-equipment',
+    templateUrl: './equipment.component.html',
+    styleUrls: ['./equipment.component.css'],
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
+    equipmentItems: object[] = [
+        { name: 'Duct Tape', mass: 0.5 },
+        { name: 'Space Camera', mass: 20 },
+        { name: 'Food', mass: 150 },
+        { name: 'Oxygen Tanks', mass: 400 },
+        { name: 'AE-35 Unit', mass: 5 },
+        { name: 'ISS Supplies', mass: 800 },
+        { name: 'Water', mass: 250 },
+        { name: 'Satellite', mass: 1200 },
+        { name: 'R2 Unit', mass: 32 },
+    ];
+    cargoHold: object[] = [];
+    cargoMass: number = 0;
+    maximumAllowedMass: number = 2000;
+    maxItems: number = 10;
 
-   constructor() { }
+    constructor() {}
 
-   ngOnInit() { }
+    ngOnInit() {}
 
-   // Code your addItem function here:
-   
+    addItem(equipment: object) {
+      this.cargoHold.push(equipment);
+      this.cargoMass += equipment.mass;
+      if ((this.maximumAllowedMass - this.cargoMass) <= 200) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    addDisabled(equipment) {
+      if ((this.cargoHold.length >= this.maxItems) || ((equipment.mass + this.cargoMass) > this.maximumAllowedMass)) {
+        return true
+    }
+  }
+
+    classActive(equipment) {
+      return ((this.cargoHold.length < this.maxItems) && ((equipment.mass + this.cargoMass) < this.maximumAllowedMass));
+    }
 }
